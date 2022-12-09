9d0ec0e85f015448f31d8d678efc3bdfc2a8097b
Alasdair Penman <alasdair.penman@hotmail.com>
2022-12-09 15:27:01 +0800

    Replaced hough with canny + circles

--- monitor/position.py
+++ monitor/position.py
@@ -6,7 +6,17 @@ import pytesseract
 
 def findPlayerPosition(img, playerID):
     button = findDealerButton(img)
-    getPlayerPositionCoordinates(img, getPlayerColour(playerID))
+
+    if button is None:
+        raise Exception("Button not found")
+
+    activePlayers = []
+    for playerX in range(1, 10):
+        playerPosition = getPlayerPositionCoordinates(img, getPlayerColour(playerX))
+        if playerPosition is not None:
+            dist = pow(pow(playerPosition[0] - button[0], 2) + pow(playerPosition[1] - button[1], 2), 0.5)
+            activePlayers.append({"player": playerX, "dist":dist})
+    print(activePlayers)
 
 
 def getPlayerPositionCoordinates(img, rbg):
@@ -23,34 +33,34 @@ def getPlayerPositionCoordinates(img, rbg):
             return [cx, cy, 7]
     return None
 
-
-
 def findDealerButton(img):
     grey = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-    circles = cv.HoughCircles(grey,cv.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=3,maxRadius=8)
-    circles = np.uint16(np.around(circles))
-    custom_config = r'--oem 3 --psm 6'
-    for i in circles[0,:]:
-        x1 = i[0] - i[2]
-        x2 = i[0] + i[2]
-        y1 = i[1] - i[2]
-        y2 = i[1] + i[2]
-        cropped = img[y1:y2,x1:x2]
-        dealerID = pytesseract.image_to_string(cropped, config=custom_config).strip()
-        if dealerID == 'D':
-            print("Found dealer")
-            return i
+
+    edges = cv.Canny(grey,50,100)
+    contours, hierarchy = cv.findContours(edges, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        approx = cv.approxPolyDP(contour, epsilon=1, closed=True)
+        area = cv.contourArea(approx)
+        if area > 150 and area < 220:
+            m = cv.moments(approx)
+            cx = int(m['m10']/m['m00'])
+            cy = int(m['m01']/m['m00'])
+            cv.circle(img, (cx,cy), 7, (255, 0, 0), 2)
+            return [cx, cy, 7]
+
+    cv.imshow('circle', img)
+    cv.waitKey(0)
     return None
 
 def getPlayerColour(playerID):
     playerIDColours = [
-            (0,0,0),
+            (0,201,183),
             (132,94,194),
             (255,83,91),
             (43,105,104),
             (189,67,48),
-            (255,151,52),
-            (0,0,0),
+            (255,146,42),
+            (255,136,151),
             (0,137,202),
             (248,190,13),
     ]
